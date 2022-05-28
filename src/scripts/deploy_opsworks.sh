0505f2b89e5f06076bdbe6d3baabc7f3b0df9568
Chris Allen <chris@apaxsoftware.com>
2022-05-28 09:09:56 -0400

    Fixes multiple target parsing

--- src/scripts/deploy_opsworks.sh
+++ src/scripts/deploy_opsworks.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2162
 
 # Compile arguments
 args=()
 (( FORCE_SETUP == 1 )) && args+=( '-f' )
-read -ar TARGETS <<< "$TARGETS"
+read -a TARGETS <<< "$TARGETS"
 args+=( '-T' "${TARGETS[@]}" )
 
 # Execute deployment
